76a03da8d0969b306566d8f2c9f0b173b8ade6b3
vineet.iitd <vineet.iitd@gmail.com>
2014-06-07 19:11:39 +0000

    Corresponding header file for MirafraHuaweiQuestions.c

---
+++ MirafraHuaweiQuestions.h
@@ -0,0 +1,20 @@
+/* File: MicrosoftAskedQuestions.h
+Description: This is the header file for the corresponding cpp file
+Author: Vineet Kumar Srivastava
+Date: 01 MAY 2010
+*/
+
+#ifndef _MIRAFRA_HUAWEI_ASKED_QUESTIONS_H_
+#define _MIRAFRA_HUAWEI_ASKED_QUESTIONS_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <conio.h>
+
+void arrangePosNegNumbers(); /* Given an array of positive and negative numbers arrange all positive number
+   and all negative numbers at one side os array */ 
+void sortCharArray(); /* A function to sort a char array */
+
+#endif
